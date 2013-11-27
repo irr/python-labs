9b32c5dba3e3642e81361e04fad67195bf7280cc
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-11-27 14:29:22 -0200

    httperf

--- gevent/web.py
+++ gevent/web.py
@@ -2,31 +2,10 @@ import gevent
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 
-import redis, umysql, json, logging, logging.handlers
+import umysql, json, logging, logging.handlers
 
-monkey.patch_all()
-
-G = { "mysql": None, 
-      "redis": None }
-
-def redis_init():
-    global G
-    try:
-        G["redis"] = redis.StrictRedis(host='localhost', port=6379, db=0)
-    except Exception as ex:
-        res = str(ex)
-        logging.error(ex)
-
-def mysql_init():
-    global G
-    try:
-        G["mysql"] = umysql.Connection()  
-        G["mysql"].connect("127.0.0.1", 3306, "root", "mysql", "mysql")
-    except Exception as ex:
-        res = str(ex)
-        logging.error(ex)
-        G["mysql"] = None        
-
+import redis
+redis.connection.socket = gevent.socket
 
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = '[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s'
@@ -37,68 +16,36 @@ logging.basicConfig(format=LOG_FORMAT)
 #logging.getLogger().addHandler(SYSLOG)
 logging.getLogger().setLevel(LOG_LEVEL)
 
-redis_init()
-mysql_init()
-
-
-def pools(names):
-    global G
-    while True:
-        try:        
-            for name in names:
-                if G[name] == None:
-                    globals()["%s_init" % name]()
-        except Exception as ex:
-            logging.error(str(ex))
-        finally:
-            gevent.sleep(10)
-
-
-def task(name, response):
-    res = None
-    try:
-        res = globals()[name](response)
-    except Exception as ex:
-        logging.error(str(ex))
-    finally:
-        return res
-
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def redis(response):
-    global G
-    ptr = G["redis"]
-    return ptr.info()
+    global r
+    return r.info()
 
 
 def mysql(response):
-    global G
-    ptr = G["mysql"]
-    if ptr == None:
-        return None
-    try:
-        rs = ptr.query("SELECT Host FROM user WHERE User = 'root'")
-        res = []
-        for h in rs.rows:
-            res.append(h[0])
-        return res
-    except Exception as ex:
-        try:
-            ptr.close()
-        finally:
-            G["mysql"] = None
-            raise ex
+    c = umysql.Connection()  
+    c.connect("127.0.0.1", 3306, "root", "mysql", "mysql")
+    rs = c.query("SELECT Host FROM user WHERE User = 'root'")
+    res = []
+    for h in rs.rows:
+        res.append(h[0])
+    c.close()
+    return res
 
 
 def application(env, start_response):
     if env["PATH_INFO"] == '/':  
         response = {}
-        g1 = gevent.spawn(task, "redis", response)
-        g2 = gevent.spawn(task, "mysql", response)
+        g1 = gevent.spawn(redis, response)
+        g2 = gevent.spawn(mysql, response)
         gevent.joinall([g1, g2])
-        response["redis"], response["mysql"] = g1.value, g2.value
         if g1.value == None or g2.value == None:
+            import sys
+            sys.exit(1)
             start_response("503 Service Unavailable", [("Content-Type", "application/json")])
             return []
+        response["2.mysql"], response["1.redis"] = g2.value, g1.value
         start_response("200 OK", [("Content-Type", "application/json")])
         return [json.dumps(response)]
     else:
@@ -108,5 +55,6 @@ def application(env, start_response):
 
 if __name__ == "__main__":
     logging.info('Listening on 8000...')
-    gevent.spawn(pools, ["redis", "mysql"])
     WSGIServer(('', 8000), application).serve_forever()
+
+# httperf --server localhost --port 8000 --num-calls 1000 --rate 100 --num-conns 100
