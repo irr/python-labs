c595ae293246a81bd9be7b89d4889ec9e2e7c406
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-12-03 06:00:25 -0200

    returncode

--- gevent/process.py
+++ gevent/process.py
@@ -2,19 +2,19 @@
 import gevent
 from gevent import subprocess
 
-
 # run 2 jobs in parallel
 p1 = subprocess.Popen(['uname'], stdout=subprocess.PIPE)
 p2 = subprocess.Popen(['ls'], stdout=subprocess.PIPE)
 
-gevent.wait([p1, p2], timeout=2)
+gevent.wait([p1, p2], timeout=10)
 
 # print the results (if available)
 if p1.poll() is not None:
-    print ('uname: %r' % p1.stdout.read())
+    print ('uname: %r (%d)' % (p1.stdout.read(), p1.returncode))
 else:
     print ('uname: job is still running')
+
 if p2.poll() is not None:
-    print ('ls: %r' % p2.stdout.read())
+    print ('ls: %r (%d)' % (p2.stdout.read(), p2.returncode))
 else:
     print ('ls: job is still running')
