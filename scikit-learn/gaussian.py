1a2f4a96168c46e414f36f8a3612889544f177d4
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-12-04 06:28:56 -0200

    nltk

--- scikit-learn/gaussian.py
+++
@@ -1,6 +0,0 @@
-from sklearn import datasets
-iris = datasets.load_iris()
-from sklearn.naive_bayes import GaussianNB
-gnb = GaussianNB()
-y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-print("Number of mislabeled points : %d" % (iris.target != y_pred).sum())
