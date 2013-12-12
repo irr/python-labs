4022e1c58b141e5933169abc32536c7285cc26f1
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-12-12 09:13:49 -0200

    redis

--- tornado/web/handlers/Index.py
+++ tornado/web/handlers/Index.py
@@ -5,6 +5,7 @@ from tornado.escape import json_encode, json_decode
 import tornado.web
 import tornado.gen
 import tornadoredis
+import torndb
 
 import time
 
@@ -14,6 +15,7 @@ class IndexHandler(RequestHandler):
     def initialize(self, **kwargs):
         self.logger = kwargs.get("logger")
         self.redis = tornadoredis.Client(connection_pool=kwargs["redis"])
+        self.mysql = torndb.Connection("localhost", "mysql")
         self.template = Template("op -cmd {{ cmd }} -value {{ value }}")
 
     def _customize(self):
@@ -22,15 +24,18 @@ class IndexHandler(RequestHandler):
 
     @tornado.web.asynchronous
     @tornado.gen.engine
+    def _redis(self):
+        info = yield tornado.gen.Task(self.redis.info)
+        yield tornado.gen.Task(self.redis.disconnect)
+        return info
+
     def _handle(self, **kwargs):
         self._customize()
         try:            
-            info = yield tornado.gen.Task(self.redis.info)
-            yield tornado.gen.Task(self.redis.disconnect)
             data = { 'cmd': "any", 'value': str(time.time()) }
             response = { 'status': 200,
                          'msg': self.template.generate(**data),
-                         'redis': info['redis_version'] }
+                         'redis': _redis['redis_version'] }
         except Exception as ex:
             response = { 'status': 500, 'msg': str(ex) }
         if response['status'] != 200:
