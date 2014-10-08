51289e086df188992047a0c8653f7a9abc406915
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-10-08 10:08:28 -0300

    srt

---
+++ utils/srt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import codecs, commands, sys, re, os
+
+def main():
+    try:
+        (_, output) = commands.getstatusoutput("file -bi \"%s\"" % (sys.argv[1],))
+        _, charset = output.split('charset=')
+        with codecs.open(sys.argv[1], 'r', encoding=charset) as source:
+            with codecs.open("%s.bak" % (sys.argv[1],), 'w+', encoding='utf8') as target:
+                target.write(source.read())
+        with codecs.open("%s.bak" % (sys.argv[1],), 'r', encoding='utf8') as source:
+            data = source.read()
+            data = re.compile(r'<[^>]+>').sub('', data)
+            with codecs.open("%s" % (sys.argv[1],), 'w+', encoding='utf8') as target:
+                target.write(data)
+        os.unlink("%s.bak" % (sys.argv[1],))
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
+if __name__ == '__main__':
+    main()
