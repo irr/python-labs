5c39aa54622319655d22c35fdbca9b596278b3d4
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-10-10 07:51:13 -0300

    gevent+only

--- bottle/web-umysql.py
+++ bottle/web-umysql.py
@@ -1,7 +1,7 @@
-from gevent.pywsgi import WSGIServer
-from cgi import parse_qs, escape
+from gevent import monkey; monkey.patch_all()
+from bottle import route, run, get, post, response, request, abort
 
-import argparse, gevent, pymysql, redis, json, logging, logging.handlers
+import sys, time, argparse, logging, logging.handlers, gevent, redis, umysql, json
 
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = '[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s'
@@ -14,43 +14,40 @@ logging.getLogger().setLevel(LOG_LEVEL)
 
 pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=100)
 
-def redis_exec():
+def redis_exec(response):
     global pool
     r = redis.Redis(connection_pool=pool)
     return r.info()
 
-def mysql_exec():
-    db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='mysql', db='mysql')
-    rs = db.cursor()
-    rs.execute("SELECT Host FROM user WHERE User = 'root' LIMIT 1")
+def mysql_exec(response):
+    db = umysql.Connection()  
+    db.connect("127.0.0.1", 3306, "root", "mysql", "mysql")
+    rs = db.query("SELECT Host FROM user WHERE User = 'root' LIMIT 1")
     res = []
-    for h in rs:
+    for h in rs.rows:
         res.append(h[0])
-    rs.close()
     db.close()
     return res
 
-def application(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'application/json; charset=utf-8')]
-
-    d = parse_qs(environ['QUERY_STRING'])
-    t = escape(d.get('t', ['0'])[0])
-
-    g1 = gevent.spawn(redis_exec)
-    g2 = gevent.spawn(mysql_exec)
-
-    if t != None and t.isdigit() and t > 0:
-        gevent.sleep(int(t))
-
-    gevent.joinall([g1, g2])
-
-    if g1.value == None or g2.value == None:
-        start_response("503", headers)
-        yield "%s\n" % json.dumps({"error": "Sorry, service unavailable."})[:1024]
+@route('/')
+@get('/')
+@post('/')
+def application():
+    response.content_type = 'application/json; charset=utf-8'
+    g1 = gevent.spawn(redis_exec, response)
+    g2 = gevent.spawn(mysql_exec, response)
+    t = 0
+    if 't' in request.query:
+        t = request.query['t'] 
     else:
-        start_response(status, headers)
-        yield "%s\n" % json.dumps({"redis": g1.value["redis_version"], "mysql":g2.value, "t":t})[:1024]
+        t = request.forms.get('t')
+    if t != None and t.isdigit() and t > 0:
+        time.sleep(int(t))
+    gevent.joinall([g1, g2])
+    if g1.value == None or g2.value == None:
+        abort(503, "Sorry, service unavailable.")
+    return "%s\n" % json.dumps({"redis": g1.value["redis_version"], "mysql":g2.value})[:1024]
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -71,4 +68,4 @@ if __name__ == "__main__":
     if args.syslog:
         logging.getLogger().addHandler(SYSLOG)
     logging.info('Listening on %s:%d...' % (args.bind, args.port))
-    WSGIServer((args.bind, args.port), application).serve_forever()
+    run(host=args.bind, port=args.port, server='gevent')
