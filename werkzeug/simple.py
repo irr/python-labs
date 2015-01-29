a3c577bca6548b253fa5267f496444559f8a0d3a
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-29 13:55:08 -0200

    python-plugin

--- werkzeug/simple.py
+++ werkzeug/simple.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('127.0.0.1', 1972, application)
 
-# uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --wsgi-file simple.py
+# uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --plugins python --wsgi-file simple.py
 
 # ab -n 10000 -c 100 http://127.0.0.1:1972/
 
-# uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --http-socket :1972 --plugins python --module=simple
+# uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --http-socket :1972 --plugins python --wsgi-file simple.py
 
-# uwsgi --socket :1972 --enable-threads --plugins python --module=simple
+# uwsgi --socket :1972 --enable-threads --plugins python --module=simple --file=/home/irocha/pyon/werkzeug/simple.py 
 # ab -n 10000 -c 100 http://127.0.0.1:8888/
 
