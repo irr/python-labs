eae231a6f2d6e193faca273f4f8b79bd9f6965ac
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-29 13:47:43 -0200

    python3+arch

--- werkzeug/simple.py
+++ werkzeug/simple.py
@@ -6,6 +6,14 @@ def application(request):
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 1972, application)
+    run_simple('127.0.0.1', 1972, application)
 
 # uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --wsgi-file simple.py
+
+# ab -n 10000 -c 100 http://127.0.0.1:1972/
+
+# uwsgi --log-format '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" status) %(size) "%(referer)"%(uagent)' -p 4 --http-socket :1972 --enable-threads --http-socket :1972 --plugins python --module=simple
+
+# uwsgi --socket :1972 --enable-threads --plugins python --module=simple
+# ab -n 10000 -c 100 http://127.0.0.1:8888/
+
