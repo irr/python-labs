46a19a1832c1c49bb2f623bffadf581980403483
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-05-07 11:27:27 -0300

    ubuntu32

--- env/install.sh
+++ env/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p ~/.python-eggs
-chmod g-wx,o-wx ~/.python-eggs
+#mkdir -p ~/.python-eggs
+#chmod g-wx,o-wx ~/.python-eggs
 mkdir -p /opt/python/github
 cd /opt/python/github
 git clone git@github.com:irr/high_performance_python.git
@@ -23,14 +23,9 @@ cd awesome-python
 git remote add upstream https://github.com/vinta/awesome-python.git
 git fetch upstream && git merge upstream/master && git push
 cd ..
-git clone git@github.com:irr/holdme.git
-cd holdme
-git remote add upstream https://github.com/ChrisBeaumont/holdme.git
-git fetch upstream && git merge upstream/master && git push
 cd ~/gitf
 ln -s /opt/python/github/awesome-python
 ln -s /opt/python/github/gevent-tutorial
 ln -s /opt/python/github/high_performance_python
-ln -s /opt/python/github/holdme
 ln -s /opt/python/github/python-guide
 cd
