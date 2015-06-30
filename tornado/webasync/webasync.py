ee7a14c72a762bf4433d3732504a94a2079c3377
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-06-30 14:09:49 -0300

    uol

--- tornado/webasync/webasync.py
+++ tornado/webasync/webasync.py
@@ -1,16 +1,19 @@
 import tornado.web
 import tornado.ioloop
 import tornado.httpclient
+import json
 
 class MyHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         def __callback(response):
-            self.write(response.body)
+            data = { 'body': response.body[:20] + '...', 'length' : len(response.body) }
+            self.set_header('Content-Type', 'application/json; charset="utf-8"')
+            self.write(json.dumps(data))
             self.finish()
 
         async_client = tornado.httpclient.AsyncHTTPClient()
-        request = tornado.httpclient.HTTPRequest("http://localhost:8085/")
+        request = tornado.httpclient.HTTPRequest("http://www.uol.com.br/")
         async_client.fetch(request, __callback) 
 
 application = tornado.web.Application([
