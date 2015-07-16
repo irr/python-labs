44e12c99cf9b97a25873b0e80680e2368d98d47e
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-07-16 16:29:24 -0300

    pylint

--- gevent/web.py
+++ gevent/web.py
@@ -1,3 +1,7 @@
+"""
+Basic template for gevent web server
+"""
+
 from gevent.pywsgi import WSGIServer
 from cgi import parse_qs, escape
 
@@ -12,63 +16,75 @@ SYSLOG.setFormatter(logging.Formatter(LOG_FORMAT))
 logging.basicConfig(format=LOG_FORMAT)
 logging.getLogger().setLevel(LOG_LEVEL)
 
-pool = redis.ConnectionPool(host='localhost', port=6379, db=0, max_connections=100)
+POOL = redis.ConnectionPool(host='localhost', port=6379,
+                            db=0, max_connections=100)
 
 def redis_exec():
-    global pool
-    r = redis.Redis(connection_pool=pool)
-    return r.info()
+    """
+    retrieve redis info array
+    """
+    connection = redis.Redis(connection_pool=POOL)
+    return connection.info()
 
 def mysql_exec():
-    db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='mysql', db='mysql')
-    rs = db.cursor()
-    rs.execute("SELECT Host FROM user WHERE User = 'root' LIMIT 1")
+    """
+    retrieve mysql hosts
+    """
+    dbase = pymysql.connect(host='127.0.0.1', port=3306,
+                            user='root', passwd='mysql', db='mysql')
+    result = dbase.cursor()
+    result.execute("SELECT Host FROM user WHERE User = 'root' LIMIT 1")
     res = []
-    for h in rs:
-        res.append(h[0])
-    rs.close()
-    db.close()
+    for host in result:
+        res.append(host[0])
+    result.close()
+    dbase.close()
     return res
 
 def application(environ, start_response):
+    """
+    application handler
+    """
     status = '200 OK'
     headers = [('Content-Type', 'application/json; charset=utf-8')]
 
-    d = parse_qs(environ['QUERY_STRING'])
-    t = int(escape(d.get('t', ['0'])[0]))
+    data = parse_qs(environ['QUERY_STRING'])
+    time = int(escape(data.get('t', ['0'])[0]))
 
-    if t > 0:
-        gevent.sleep(int(t))
+    if time > 0:
+        gevent.sleep(int(time))
 
-    g1 = gevent.spawn(redis_exec)
-    g2 = gevent.spawn(mysql_exec)
+    gev1 = gevent.spawn(redis_exec)
+    gev2 = gevent.spawn(mysql_exec)
 
-    gevent.joinall([g1, g2])
+    gevent.joinall([gev1, gev2])
 
-    if g1.value == None or g2.value == None:
+    if gev1.value == None or gev2.value == None:
         start_response("503", headers)
-        yield ("%s\n" % json.dumps({"error": "Sorry, service unavailable."})[:1024]).encode('utf-8')
+        yield ("%s\n" % json.dumps(
+            {"error": "Sorry, service unavailable."})[:1024]).encode('utf-8')
     else:
         start_response(status, headers)
-        yield ("%s\n" % json.dumps({"redis": g1.value["redis_version"], "mysql":g2.value, "t":t})[:1024]).encode('utf-8')
+        yield ("%s\n" % json.dumps({"redis": gev1.value["redis_version"],
+                                    "mysql":gev2.value, "t":time})[:1024]).encode('utf-8')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--syslog', 
-                        dest="syslog", 
-                        action='store_true', 
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-s', '--syslog',
+                        dest="syslog",
+                        action='store_true',
                         help="enable syslog (default: disabled)")
-    parser.add_argument('-b', '--bind', 
-                        dest="bind", 
+    PARSER.add_argument('-b', '--bind',
+                        dest="bind",
                         default="0.0.0.0",
                         help="bind address (default: 0.0.0.0)")
-    parser.add_argument('-p', '--port', 
-                        dest="port", 
+    PARSER.add_argument('-p', '--port',
+                        dest="port",
                         type=int,
                         default=8000,
                         help="listen port (default: 8000)")
-    args = parser.parse_args()
-    if args.syslog:
+    ARGS = PARSER.parse_args()
+    if ARGS.syslog:
         logging.getLogger().addHandler(SYSLOG)
-    logging.info('Listening on %s:%d...' % (args.bind, args.port))
-    WSGIServer((args.bind, args.port), application).serve_forever()
+    logging.info('Listening on %s:%d...' % (ARGS.bind, ARGS.port))
+    WSGIServer((ARGS.bind, ARGS.port), application).serve_forever()
