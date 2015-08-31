267a2e95ba55766aae00f4123f167b2977d8011e
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-08-31 13:30:46 -0300

    echo

---
+++ gevent/echo2.py
@@ -0,0 +1,11 @@
+from gevent.server import StreamServer
+
+def connection_handler(socket, address):
+    for l in socket.makefile('r'):
+        socket.sendall(l)
+
+
+if __name__ == '__main__':
+    server = StreamServer(('0.0.0.0', 8000), connection_handler)
+    server.serve_forever()
+
