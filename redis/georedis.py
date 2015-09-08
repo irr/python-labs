ea6676044e69e0dda4f257a9205013189d05f26e
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-08 18:28:59 -0300

    python3

--- redis/georedis.py
+++ redis/georedis.py
@@ -8,12 +8,12 @@ red = redis.StrictRedis(connection_pool=pool)
 red.execute_command("GEOADD Sicily 13.361389 38.115556 Palermo")
 red.execute_command("GEOADD Sicily 15.087269 37.502669 Catania")
 
-print red.execute_command("GEOHASH Sicily Palermo")
+print(red.execute_command("GEOHASH Sicily Palermo"))
 
-print red.execute_command("GEORADIUS Sicily 15 37 200 km WITHDIST")
+print(red.execute_command("GEORADIUS Sicily 15 37 200 km WITHDIST"))
 
-print red.execute_command("GEORADIUS Sicily 15 37 200 km WITHCOORD")
+print(red.execute_command("GEORADIUS Sicily 15 37 200 km WITHCOORD"))
 
-print red.execute_command("GEORADIUS Sicily 15 37 200 km WITHDIST WITHCOORD")
+print(red.execute_command("GEORADIUS Sicily 15 37 200 km WITHDIST WITHCOORD"))
 
-print red.execute_command("GEODIST Sicily Palermo Catania km")
+print(red.execute_command("GEODIST Sicily Palermo Catania km"))
