f741a0f2925731d02b299f6858d585a3cdf2387b
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-17 09:02:13 -0300

    uninstall

---
+++ env/uninstall-2.7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /usr/local/bin
+sudo rm -rf 2to3 easy_install easy_install-2.7 idle pip pip2 pip2.7 pydoc python2.7 python2.7-config smtpd.py virtualenv*
+cd /usr/local/lib
+sudo rm -rf libpython2.7* python2.7
+sudo rm -rf /usr/local/share/man/man1/python2.7*
+
