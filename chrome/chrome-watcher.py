56a7c989bc25b79acd038ccbf0a47e8d616fe3c2
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-17 23:59:02 -0300

    syslog

--- chrome/chrome-watcher.py
+++ chrome/chrome-watcher.py
@@ -1,13 +1,22 @@
+import logging.handlers
 import psutil
 import time
 
-while not time.sleep(0.5):
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = '[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s'
+
+SYSLOG = logging.handlers.SysLogHandler(address='/dev/log')
+SYSLOG.setFormatter(logging.Formatter(LOG_FORMAT))
+
+logging.basicConfig(format=LOG_FORMAT)
+logging.getLogger().setLevel(LOG_LEVEL)
+logging.getLogger().addHandler(SYSLOG)
+
+while not time.sleep(1):
     chrome = { 'proc': None, 'zombie': False }
     chromes = []
 
-    processes = [p for p in psutil.process_iter() if p.name() == 'chrome']
-
-    for proc in processes:
+    for proc in psutil.process_iter():
         try:
             if proc.name() == 'chrome':
                 if proc.status() == psutil.STATUS_ZOMBIE:
@@ -18,8 +27,11 @@ while not time.sleep(0.5):
         except psutil.NoSuchProcess:
             pass
 
+
     if chrome['zombie']:
+        logging.getLogger().warn("killing zombie process: [%s]" % (chrome,))
         chrome['proc'].kill()
     elif len(chromes) == 1:
+        logging.getLogger().warn("killing sleeping process: [%s]" % (chromes[0],))
         chromes[0].kill()
 
