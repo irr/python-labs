591823484b0e5b4f829907e3edc89728df420b66
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-11-06 13:03:41 -0200

    ubuntu

--- env/install.sh
+++ env/install.sh
@@ -24,11 +24,6 @@ cd awesome-python
 git remote add upstream https://github.com/vinta/awesome-python.git
 git fetch upstream && git merge upstream/master && git push
 cd ..
-git clone git@github.com:irr/acitoolkit.git
-cd acitoolkit
-git remote add upstream https://github.com/datacenter/acitoolkit.git
-git fetch upstream && git merge upstream/master && git push
-cd ..
 git clone git@github.com:irr/pyinstaller.git
 cd pyinstaller
 git remote add upstream https://github.com/pyinstaller/pyinstaller.git
@@ -38,6 +33,5 @@ ln -s /opt/python/github/awesome-python
 ln -s /opt/python/github/gevent-tutorial
 ln -s /opt/python/github/high_performance_python
 ln -s /opt/python/github/python-guide
-ln -s /opt/python/github/acitoolkit
 ln -s /opt/python/github/pyinstaller
 cd
