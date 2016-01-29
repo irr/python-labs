f1f6dc07ba5f7ad1659a867df0c093851a86a928
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-01-28 22:56:58 -0200

    signals

--- threads/prodcons.py
+++ threads/prodcons.py
@@ -1,4 +1,4 @@
-import random, time, threading
+import random, signal, sys, time, threading
 
 class Producer(threading.Thread):
     """
@@ -15,7 +15,7 @@ class Producer(threading.Thread):
         threading.Thread.__init__(self)
         self.integers = integers
         self.condition = condition
-    
+
     def run(self):
         """
         Thread run method. Append random integers to the integers list
@@ -25,7 +25,7 @@ class Producer(threading.Thread):
             integer = random.randint(0, 256)
             self.condition.acquire()
             print 'condition acquired by %s' % self.name
-            self.integers.append(integer) 
+            self.integers.append(integer)
             print '%d appended to list by %s' % (integer, self.name)
             print 'condition notified by %s' % self.name
             self.condition.notify()
@@ -48,7 +48,7 @@ class Consumer(threading.Thread):
         threading.Thread.__init__(self)
         self.integers = integers
         self.condition = condition
-    
+
     def run(self):
         """
         Thread run method. Consumes integers from list
@@ -66,15 +66,30 @@ class Consumer(threading.Thread):
             print 'condition released by %s' % self.name
             self.condition.release()
 
+
+def sigterm_handler(signo, _):
+    print "Ctrl+C (%d) captured..." % (signo,)
+    sys.exit(0)
+
 def main():
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
     integers = []
     condition = threading.Condition()
     t1 = Producer(integers, condition)
     t2 = Consumer(integers, condition)
+    t1.setDaemon(True)
+    t2.setDaemon(True)
     t1.start()
     t2.start()
-    t1.join()
-    t2.join()
+
+    try:
+        while t1.is_alive():
+            t1.join(1)
+        while t2.is_alive():
+            t2.join(1)
+    except (KeyboardInterrupt, SystemExit) as err:
+        sigterm_handler(signal.SIGTERM, None)
 
 if __name__ == '__main__':
     main()
