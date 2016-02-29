cb4122189c4d52aa27547eb3bca94f1b56179d31
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-02-29 13:43:00 -0300

    dev

--- fpm/dev.sh
+++ fpm/dev.sh
@@ -4,6 +4,7 @@ NAME=pydev
 DEB=${NAME}_${VERSION}_amd64.deb
 RPM=${NAME}-${VERSION}-1.x86_64.rpm
 PACKAGE=${DEB}
+DIST=dev
 
 echo "building ${PACKAGE}"
 
@@ -12,14 +13,15 @@ rm -rf ${PACKAGE}
 if [[ "${PACKAGE}" =~ "rpm$" ]]
     then
         fpm -s dir -t rpm -n ${NAME} -v ${VERSION} --rpm-user irocha --rpm-group irocha \
-            --prefix /home/irocha/dev \
-            --description "virtualenv (dev)" -m "ivan.ribeiro@gmail.com" \
-            -C $HOME/dev && \
+            --prefix /home/irocha/${DIST} \
+            --description "virtualenv (${DIST})" -m "ivan.ribeiro@gmail.com" \
+            -C $HOME/${DIST} && \
         rpm -q -filesbypkg -p ${PACKAGE}
     else
         fpm -s dir -t deb -n ${NAME} -v ${VERSION} --deb-user irocha --deb-group irocha \
-            --prefix /home/irocha/dev \
-            --description "virtualenv (dev)" -m "ivan.ribeiro@gmail.com" \
-            -C $HOME/dev && \
+            --prefix /home/irocha/${DIST} \
+            --description "virtualenv (${DIST})" -m "ivan.ribeiro@gmail.com" \
+            -d "libpython2.7" \
+            -C $HOME/${DIST} && \
         dpkg -c ${PACKAGE}
 fi
