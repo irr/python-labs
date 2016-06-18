501f251483a27e274057eefceb0993d0e53c9c5c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-06-18 08:35:30 -0300

    base62

---
+++ base62/base62.py
@@ -0,0 +1,21 @@
+BASE_ALPH = tuple("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+BASE_DICT = dict((c, v) for v, c in enumerate(BASE_ALPH))
+BASE_LEN = len(BASE_ALPH)
+
+def base_decode(string):
+    num = 0
+    for char in string:
+        num = num * BASE_LEN + BASE_DICT[char]
+    return num
+
+def base_encode(num):
+    if not num:
+        return BASE_ALPH[0]
+
+    encoding = ""
+    while num:
+        num, rem = divmod(num, BASE_LEN)
+        encoding = BASE_ALPH[rem] + encoding
+    return encoding
+
+print(base_encode(10101010101010101010))
