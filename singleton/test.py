be91bf0487285397b52d04c5d8482b2c4a2f64dc
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-10-12 10:52:25 -0300

    singleton

---
+++ singleton/test.py
@@ -0,0 +1,13 @@
+def singleton(class_):
+  instances = {}
+  def getinstance(*args, **kwargs):
+    if class_ not in instances:
+        instances[class_] = class_(*args, **kwargs)
+    return instances[class_]
+  return getinstance
+
+@singleton
+class MyClass():
+  def test(self):
+    print(id(self))
+
