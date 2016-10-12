2bce2df83b68264a152ebc013ed820c6a387831c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-10-12 10:55:49 -0300

    class

--- singleton/test.py
+++ singleton/test.py
@@ -11,3 +11,14 @@ class MyClass():
   def test(self):
     print(id(self))
 
+
+class Singleton(object):
+  _instance = None
+  def __new__(class_, *args, **kwargs):
+    if not isinstance(class_._instance, class_):
+        class_._instance = object.__new__(class_, *args, **kwargs)
+    return class_._instance
+
+class MyRealClass(Singleton):
+  def test(self):
+    print(id(self))
