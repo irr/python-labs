1c8e39ed06c2210f42e8602a4a5e29254cec47af
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2017-11-29 11:43:54 -0200

    count

--- sns/count.py
+++ sns/count.py
@@ -1,18 +1,43 @@
 import arrow
 import boto3
+import json
 
 client = boto3.client('sns')
 
-nt = ""
 
-now = arrow.utcnow().float_timestamp * 1000
+def get_topic_arn(topic):
+    global client
 
-while True:
-    subs = client.list_subscriptions(NextToken=nt)
-    nt = subs.get("NextToken", "")
-    if nt is "":
-        break
-    print(len(subs["Subscriptions"]))
+    arn = None
+    nt = ""
+    arns = {}
 
-timespent = arrow.utcnow().float_timestamp * 1000 - now
-print(timespent)
+    while True:
+        result = client.list_topics(NextToken=nt)
+        index = { topic["TopicArn"].split(":")[-1]: topic["TopicArn"] for topic in result.get("Topics", [])}
+        arns = { **index, **arns }
+        nt = result.get("NextToken", "")
+        if nt is "":
+            break
+
+    return arns.get(topic, None)
+
+
+def count(topic):
+    global client
+
+    now = arrow.utcnow().float_timestamp
+    total = 0
+    topic = get_topic_arn(topic)
+    if topic is None:
+         return {"total": total, "timespent": arrow.utcnow().float_timestamp - now}
+
+    nt = ""
+    while True:
+        subs = client.list_subscriptions_by_topic(TopicArn=topic, NextToken=nt)
+        total += len(subs.get("Subscriptions", 0))
+        nt = subs.get("NextToken", "")
+        if nt is "":
+            break
+
+    return {"total": total, "timespent": arrow.utcnow().float_timestamp - now}
