242b1c127158ddb1bea2bbe3e89a5100d04b7d93
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2018-05-08 10:22:41 -0300

    gzip+ok

--- chalice/app.py
+++ chalice/app.py
@@ -12,8 +12,42 @@ def index():
 
 """
 
-aws apigateway update-rest-api --rest-api-id 2zwhyrcycf --patch-operations op=replace,path=/minimumCompressionSize,value='0'
+aws apigateway update-rest-api --rest-api-id 2mk0o5c691 --patch-operations op=replace,path=/minimumCompressionSize,value='0'
 
-http -v https://2zwhyrcycf.execute-api.us-east-1.amazonaws.com/api/
+(dev3) [irocha@irrlab chalice (master)]$ aws apigateway create-deployment --rest-api-id 2mk0o5c691 --region us-east-1 --stage-name api
+{
+    "id": "tdsrc8",
+    "createdDate": 1525785583
+}
+(dev3) [irocha@irrlab chalice (master)]$ aws apigateway update-stage --region us-east-1 --rest-api-id 2mk0o5c691 --stage-name api --patch-operations op='replace',path='/deploymentId',value='tdsrc8'
+{
+    "deploymentId": "tdsrc8",
+    "stageName": "api",
+    "cacheClusterEnabled": false,
+    "cacheClusterStatus": "NOT_AVAILABLE",
+    "methodSettings": {},
+    "createdDate": 1525785504,
+    "lastUpdatedDate": 1525785624
+}
+
+(dev3) [irocha@irrlab chalice (master)]$ aws apigateway get-deployments --rest-api-id 2mk0o5c691 --region us-east-1
+{
+    "items": [
+        {
+            "id": "ach8yw",
+            "createdDate": 1525785504
+        },
+        {
+            "id": "tdsrc8",
+            "createdDate": 1525785583
+        }
+    ]
+}
+
+aws apigateway get-deployments --rest-api-id 2mk0o5c691 --region us-east-1
+
+aws apigateway delete-deployment --rest-api-id 2mk0o5c691 --deployment-id ach8yw --region us-east-1
+
+http -v https://2mk0o5c691.execute-api.us-east-1.amazonaws.com/api/
 
 """
