19fcaf142484727eea7f1e4a57dc1dde7c062444
Ivan Ribeiro Rocha <ivan@flaner.com>
2019-01-24 10:16:04 -0200

    bugfix

--- dynamodb/lock.py
+++ dynamodb/lock.py
@@ -1,27 +1,36 @@
 import boto3
 import decimal
 import time
+import sys
 
 from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('prd')
+table = dynamodb.Table('ncds-db')
 
 now = int(time.time())
 
 expiration_secs = 5
 
 try:
-    res = table.update_item(
-        Key={"h": "irrlab", "r": "lock"},
-        UpdateExpression="set c = :val",
-        ExpressionAttributeValues={":val": decimal.Decimal(now + expiration_secs), ":cond": decimal.Decimal(now)},
-        ConditionExpression="c < :cond",
-        ReturnValues="UPDATED_NEW"
+    res = table.put_item(
+        Item={"k": "irrlab2", "t": decimal.Decimal(now + expiration_secs)},
+        ConditionExpression="attribute_not_exists(k)",
+        ReturnValues="NONE"
     )
     print(f"now={now}, exp={expiration_secs}", res)
 except ClientError as e:
-    print(f"now={now}, exp={expiration_secs} locked!", e.__class__.__name__ == "ConditionalCheckFailedException")
+    try:
+        res = table.update_item(
+            Key={"k": "irrlab2"},
+            UpdateExpression="set t = :val",
+            ExpressionAttributeValues={":val": decimal.Decimal(now + expiration_secs), ":cond": decimal.Decimal(now)},
+            ConditionExpression="t < :cond",
+            ReturnValues="UPDATED_NEW"
+        )
+        print(f"now={now}, exp={expiration_secs}", res)
+    except ClientError as e:
+        print(f"now={now}, exp={expiration_secs} locked!", e.__class__.__name__ == "ConditionalCheckFailedException")
 
 """
 now=1548308211, exp=5 {'Attributes': {'c': Decimal('1548308216')}, 'ResponseMetadata': {'RequestId': 'T47Q58T4REEVNOEP5Q2OR7H7ARVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:36:52 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'T47Q58T4REEVNOEP5Q2OR7H7ARVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '2048102356'}, 'RetryAttempts': 0}}
