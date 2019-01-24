518a6db96d1cced81a876bcb1d397e26e265d983
Ivan Ribeiro Rocha <ivan@flaner.com>
2019-01-24 03:37:29 -0200

    debug

--- dynamodb/lock.py
+++ dynamodb/lock.py
@@ -19,18 +19,13 @@ try:
         ConditionExpression="c < :cond",
         ReturnValues="UPDATED_NEW"
     )
-    print(res)
+    print(f"now={now}, exp={expiration_secs}", res)
 except ClientError as e:
-    print("locked!", e.__class__.__name__ == "ConditionalCheckFailedException")
+    print(f"now={now}, exp={expiration_secs} locked!", e.__class__.__name__ == "ConditionalCheckFailedException")
 
 """
-(venv) [irocha@irrlab dynamodb (master)]$ python lock.py ; sleep 3; python lock.py; sleep 2; python lock.py
-{'Attributes': {'c': Decimal('1548307599')}, 'ResponseMetadata': {'RequestId': 'GJJSE9JLEIPKPIMRC438JHOLDRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:26:35 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'GJJSE9JLEIPKPIMRC438JHOLDRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '144160330'}, 'RetryAttempts': 0}}
-locked! True
-{'Attributes': {'c': Decimal('1548307606')}, 'ResponseMetadata': {'RequestId': 'SRRUGIQCG464G9761EBJNS6MRBVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:26:41 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'SRRUGIQCG464G9761EBJNS6MRBVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '2619370190'}, 'RetryAttempts': 0}}
-(venv) [irocha@irrlab dynamodb (master)]$ python lock.py; sleep 3; python lock.py; sleep 2; python lock.py; sleep 1; python lock.py
-{'Attributes': {'c': Decimal('1548307623')}, 'ResponseMetadata': {'RequestId': '8ES3MLVMTQN2TMDBPQGQEHSEVBVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:26:59 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': '8ES3MLVMTQN2TMDBPQGQEHSEVBVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '419972277'}, 'RetryAttempts': 0}}
-locked! True
-{'Attributes': {'c': Decimal('1548307630')}, 'ResponseMetadata': {'RequestId': 'GRPLAOF2S1NQVOARH2AA2DC6VJVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:27:06 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'GRPLAOF2S1NQVOARH2AA2DC6VJVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '2515856832'}, 'RetryAttempts': 0}}
-locked! True
+now=1548308211, exp=5 {'Attributes': {'c': Decimal('1548308216')}, 'ResponseMetadata': {'RequestId': 'T47Q58T4REEVNOEP5Q2OR7H7ARVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:36:52 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'T47Q58T4REEVNOEP5Q2OR7H7ARVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '2048102356'}, 'RetryAttempts': 0}}
+now=1548308215, exp=5 locked! True
+now=1548308218, exp=5 {'Attributes': {'c': Decimal('1548308223')}, 'ResponseMetadata': {'RequestId': 'HICH3HG1D7MAQCDRP94D7U3AKFVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Thu, 24 Jan 2019 05:36:59 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '39', 'connection': 'keep-alive', 'x-amzn-requestid': 'HICH3HG1D7MAQCDRP94D7U3AKFVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '879194634'}, 'RetryAttempts': 0}}
+now=1548308221, exp=5 locked! True
 """
