ca80452390e828cca904441d0bddde7d5b056947
Ivan Ribeiro Rocha <ivan.ribeiro@gmail.com>
2019-08-25 23:47:12 +0100

    retab

--- pytesting/tests/conftest.py
+++ pytesting/tests/conftest.py
@@ -3,17 +3,17 @@ import pytest
 
 @pytest.fixture
 def data_series():
-	with open("tests/data.csv", "r", newline="") as file:
-		return list(csv.reader(file, delimiter=","))
+    with open("tests/data.csv", "r", newline="") as file:
+        return list(csv.reader(file, delimiter=","))
 
 
 # fixture composed by another fixture
 @pytest.fixture
 def my_data_series(data_series):
-	return [x for x in data_series if x[0] == "twitter"]
+    return [x for x in data_series if x[0] == "twitter"]
 
 
 @pytest.fixture
 def app():
-	from app import application
-	return application
+    from app import application
+    return application
--- pytesting/tests/test_tips.py
+++ pytesting/tests/test_tips.py
@@ -24,25 +24,25 @@ random.seed()
 def delay(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-    	secs = round(random.uniform(0.3, 0.5), 2)
-    	params = {"args": list(*args), "kwargs": dict(**kwargs)}
-    	print(f"{func.__name__}[{params}] [wait={secs}]")
-    	time.sleep(secs)
-    	return func(*args, **kwargs)
+        secs = round(random.uniform(0.3, 0.5), 2)
+        params = {"args": list(*args), "kwargs": dict(**kwargs)}
+        print(f"{func.__name__}[{params}] [wait={secs}]")
+        time.sleep(secs)
+        return func(*args, **kwargs)
     return wrapper_timer
 
 
 @delay
 def test_approx_dict():
-	values = {'v1': 0.1 + 1.2, 'v2': 0.2 + 0.8}
-	assert values == pytest.approx(dict(v1=1.3, v2=1.0))
+    values = {'v1': 0.1 + 1.2, 'v2': 0.2 + 0.8}
+    assert values == pytest.approx(dict(v1=1.3, v2=1.0))
 
 
 @pytest.mark.ml
 @delay
 def test_approx_numpy():
-	values = np.array([0.1, 0.2]) + np.array([1.2, 0.8])
-	assert values == pytest.approx(np.array([1.3, 1.0]))
+    values = np.array([0.1, 0.2]) + np.array([1.2, 0.8])
+    assert values == pytest.approx(np.array([1.3, 1.0]))
 
 
 # test MUST fail!
@@ -50,46 +50,46 @@ def test_approx_numpy():
 @pytest.mark.ml
 @delay
 def test_numpy():
-	values = np.array([0.1, 0.2]) + np.array([1.2, 0.8])
-	assert values == np.array([1.3, 1.0])
+    values = np.array([0.1, 0.2]) + np.array([1.2, 0.8])
+    assert values == np.array([1.3, 1.0])
 
 
 @pytest.mark.parametrize(
-	"data, result",
-	[
-		((8, 4), 12.0),
-		pytest.param(
-			(1, 3), 4.0,
-			marks=pytest.mark.ml,
-			id="myparamtest"
-			# call with: pytest -k myparamtest
-		),
-	],
+    "data, result",
+    [
+        ((8, 4), 12.0),
+        pytest.param(
+            (1, 3), 4.0,
+            marks=pytest.mark.ml,
+            id="myparamtest"
+            # call with: pytest -k myparamtest
+        ),
+    ],
 )
 @delay
 def test_parametrize(data, result):
-	values = sum(data)
-	assert values == pytest.approx(result)
+    values = sum(data)
+    assert values == pytest.approx(result)
 
 
 @delay
 @pytest.mark.csv
 def test_fixture_data(data_series):
-	# call with: pytest -m csv
-	assert len(data_series) == 4
+    # call with: pytest -m csv
+    assert len(data_series) == 4
 
 
 @delay
 @pytest.mark.csv
 def test_fixture_mydata(my_data_series):
-	# call with: pytest -m csv
-	assert len(my_data_series) == 1
+    # call with: pytest -m csv
+    assert len(my_data_series) == 1
 
 
 @pytest.mark.web
 def test_web_handler(client):
-	response = client.get("/")
-	assert response.status_code == 200
-	mapping = response.json
-	for site in WEBSITES:
-		assert mapping[site]["status"] == 200
+    response = client.get("/")
+    assert response.status_code == 200
+    mapping = response.json
+    for site in WEBSITES:
+        assert mapping[site]["status"] == 200
