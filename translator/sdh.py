b4bc46f10fdf4cbb74b1fdba5e5f5c8d644e34e6
Ivan Ribeiro Rocha <ivan.ribeiro@gmail.com>
2024-09-14 12:41:29 +0100

    fix

--- translator/sdh.py
+++ translator/sdh.py
@@ -5,9 +5,9 @@ import argparse
 def remove_sdh_from_subtitle(input_filename):
     with open(input_filename, "r", encoding="utf-8") as infile:
         for line in infile:
-            # Remove text within brackets []
-            cleaned_line = re.sub(r'\[.*?\]', '', line)
-            sys.stdout.write(cleaned_line)
+            for exp in [r'\[.*?\]', r'\(.*?\)', r'--(?!>)']:
+                line = re.sub(exp, '', line)
+            sys.stdout.write(line)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Remove SDH tags from an SRT subtitle file.')
